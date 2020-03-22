a4827e9fb1c59bbe3ef23a9958a878b69e5a061d
keunseok <keunseok@oraorasoft.co.kr>
2020-03-22 16:03:34 +0900

    Spring security google oauth2 기능 구현

--- src/main/java/com/kkamdung/springboot/web/IndexController.java
+++ src/main/java/com/kkamdung/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.kkamdung.springboot.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.kkamdung.springboot.config.auth.dto.SessionUser;
 import com.kkamdung.springboot.service.PostsService;
 import com.kkamdung.springboot.web.dto.PostsResponseDto;
 
@@ -15,10 +18,17 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 
 	private final PostsService postsService;
+	private final HttpSession session;
 	
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("posts", postsService.findAllDesc());
+		
+		SessionUser user = (SessionUser) session.getAttribute("user");
+		if (user != null) {
+			model.addAttribute("userName", user.getName());
+		}
+		
 		return "index";
 	}
 	
